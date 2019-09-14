882a7008e33c249c908d6bed7142ec49f12a2bc8
nanamin <nanamin0634@gmail.com>
2019-09-15 02:19:21 +0900

    add ParameterMissing exception handling (#16)
    merge for issue/15

--- app/controllers/concerns/exception_rescuable.rb
+++ app/controllers/concerns/exception_rescuable.rb
@@ -7,12 +7,19 @@ module ExceptionRescuable
     include ErrorRenderable
 
     rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+    rescue_from ActionController::ParameterMissing, with: :parameter_missing
   end
 
   def record_not_found
     render404(
       title: 'Not Found',
       messages: 'Record not found'
+    )
+  end
+
+  def parameter_missing
+    render400(
+      messages: 'パラメータが不足しています'
     )
   end
 end
