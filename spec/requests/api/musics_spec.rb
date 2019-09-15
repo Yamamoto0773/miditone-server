111e62f8dc9f723efd6f404cf560534f4a9cbebd
nanamin <nanamin0634@gmail.com>
2019-09-15 19:55:31 +0900

    add played times api

--- spec/requests/api/musics_spec.rb
+++ spec/requests/api/musics_spec.rb
@@ -60,4 +60,44 @@ RSpec.describe 'Musics', type: :request do
       is_expected.to eq 204
     end
   end
+
+  describe 'GET /api/musics/played_times' do
+    let!(:users) { create_list(:user, 3) }
+    let!(:musics) { create_list(:music, 3) }
+    before do
+      create(:score, music_id: musics[0].id, user_id: users[0].id, difficulty: :easy, played_times: 5)
+      create(:score, music_id: musics[0].id, user_id: users[1].id, difficulty: :normal)
+      create(:score, music_id: musics[0].id, user_id: users[2].id, difficulty: :hard)
+      create(:score, music_id: musics[1].id, user_id: users[0].id, difficulty: :hard)
+      create(:score, music_id: musics[2].id, user_id: users[1].id, difficulty: :hard)
+    end
+
+    it 'return played times of three musics' do
+      is_expected.to eq 200
+      expect(json[musics[0].id.to_s]).to eq 7
+      expect(json[musics[1].id.to_s]).to eq 1
+      expect(json[musics[2].id.to_s]).to eq 1
+    end
+  end
+
+  describe 'GET /api/musics/:music_id/played_times' do
+    let!(:users) { create_list(:user, 3) }
+    let!(:music) { create(:music) }
+    before do
+      create(:score, music_id: music.id, user_id: users[0].id, difficulty: :easy, played_times: 5)
+      create(:score, music_id: music.id, user_id: users[1].id, difficulty: :normal)
+      create(:score, music_id: music.id, user_id: users[2].id, difficulty: :hard)
+      create(:score, music_id: music.id, user_id: users[0].id, difficulty: :hard)
+      create(:score, music_id: music.id, user_id: users[1].id, difficulty: :hard)
+    end
+    let(:music_id) { music.id }
+
+    it 'return played times of three difficulties' do
+      is_expected.to eq 200
+      expect(json['total']).to eq 9
+      expect(json['easy']).to eq 5
+      expect(json['normal']).to eq 1
+      expect(json['hard']).to eq 3
+    end
+  end
 end
