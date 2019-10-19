463bcf05860ac9a4e35fa1fd18671791aadfb6cd
nanamin <nanamin0634@gmail.com>
2019-10-19 14:48:29 +0900

    install bullet and resolve N+1

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -47,4 +47,11 @@ Rails.application.configure do
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
+
+  # bullet
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.bullet_logger = true
+    Bullet.raise = false # raise an error if n+1 query occurs
+  end
 end
