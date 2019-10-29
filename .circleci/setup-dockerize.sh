f69a3266e77948bf6c2171de17554076d8504665
nanamin <nanamin0634@gmail.com>
2019-10-30 01:07:58 +0900

    fix

--- .circleci/setup-dockerize.sh
+++ .circleci/setup-dockerize.sh
@@ -4,4 +4,4 @@ readonly DOCKERIZE_VERSION=v0.6.1
 
 wget https://github.com/jwilder/dockerize/releases/download/$DOCKERIZE_VERSION/dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz
 sudo tar -C /usr/local/bin -xzvf dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz
-sudo rm dockerize-linux-amd64-$DOCKERIZE_V
+sudo rm dockerize-linux-amd64-$DOCKERIZE_VERSION
